9cf20af734faadb0e06b4c288949a40ed7e9615b
Alan Rynne <alan@rynne.es>
2020-06-13 13:38:17 +0200

    Improved cylinder and sphere tests

--- src/Geometry/3D/Primitives/Cylinder.cs
+++ src/Geometry/3D/Primitives/Cylinder.cs
@@ -1,3 +1,4 @@
+using System;
 using Paramdigma.Core.Collections;
 using Paramdigma.Core.Geometry.Interfaces;
 
@@ -14,11 +15,20 @@ namespace Paramdigma.Core.Geometry
         /// <param name="plane">The plane of the cylinder.</param>
         /// <param name="radius">The radius of the cylinder.</param>
         /// <param name="domain">The cylinder height range.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when radius is smaller or equal to 0</exception>
+        /// <exception cref="ArgumentException">Throws when the height domain is tiny.</exception>
+        /// <exception cref="ArgumentNullException">Throws when the passed plane is null</exception>
         public Cylinder(Plane plane, double radius, Interval domain)
         {
-            Plane = plane;
+            if (radius <= 0)
+                throw new ArgumentOutOfRangeException(nameof(radius));
+            if (domain.Length < Settings.Tolerance)
+                throw new ArgumentException("Height length is tiny.");
+            Plane = plane ?? throw new ArgumentNullException(nameof(plane));
             Radius = radius;
             HeightRange = domain;
+            DomainU = Interval.Unit;
+            DomainV = Interval.Unit;
         }
 
         /// <summary>
@@ -27,7 +37,8 @@ namespace Paramdigma.Core.Geometry
         /// <value><see cref="Plane"/>.</value>
         public Plane Plane
         {
-            get; set;
+            get;
+            set;
         }
 
         /// <summary>
@@ -36,7 +47,8 @@ namespace Paramdigma.Core.Geometry
         /// <value><see cref="double"/>.</value>
         public double Radius
         {
-            get; set;
+            get;
+            set;
         }
 
         /// <summary>
@@ -45,7 +57,8 @@ namespace Paramdigma.Core.Geometry
         /// <value><see cref="Interval"/>.</value>
         public Interval HeightRange
         {
-            get; set;
+            get;
+            set;
         }
 
         /// <summary>
@@ -56,18 +69,21 @@ namespace Paramdigma.Core.Geometry
         /// <inheritdoc/>
         public Interval DomainU
         {
-            get; set;
+            get;
+            set;
         }
 
         /// <inheritdoc/>
         public Interval DomainV
         {
-            get; set;
+            get;
+            set;
         }
 
         /// <inheritdoc/>
         public Plane FrameAt(double u, double v)
         {
+            CheckParameters(u,v);
             throw new System.NotImplementedException();
         }
 
@@ -83,18 +99,38 @@ namespace Paramdigma.Core.Geometry
         /// </summary>
         /// <param name="point">Point to compute with.</param>
         /// <returns>Point3d instance of the closest point in the cylinder.</returns>
-        public Point3d ClosestPointTo(Point3d point) => throw new System.NotImplementedException();
+        public Point3d ClosestPointTo(Point3d point)
+        {
+            throw new NotImplementedException();
+        }
 
         /// <inheritdoc/>
         public Vector3d NormalAt(double u, double v)
         {
-            throw new System.NotImplementedException();
+            CheckParameters(u,v);
+            throw new NotImplementedException();
         }
 
         /// <inheritdoc/>
         public Point3d PointAt(double u, double v)
         {
-            throw new System.NotImplementedException();
+            CheckParameters(u, v);
+
+            double x, y, z;
+            var rho = Radius;
+            var phi = DomainU.Remap(u, new Interval(0, 2 * Math.PI));
+            x = rho * Math.Cos(phi);
+            y = rho * Math.Sin(phi);
+            z = v;
+            return Plane.PointAt(x, y, z);
+        }
+
+        private void CheckParameters(double u, double v)
+        {
+            if (!DomainU.Contains(u))
+                throw new Exception("Parameter U must be contained inside domain");
+            if (!DomainV.Contains(v))
+                throw new Exception("Parameter V must be contained inside domain");
         }
     }
 }
--- tests/Geometry/3D/CylinderTests.cs
+++ tests/Geometry/3D/CylinderTests.cs
@@ -13,7 +13,24 @@ namespace Paramdigma.Core.Tests.Geometry._3D
             Assert.Equal(Plane.WorldXY,cyl.Plane);
             Assert.Equal(1,cyl.Radius);
             Assert.Equal(1,cyl.Height);
-                
+        }
+        
+        [Fact]
+        public void CanCreate_FromDefaultConstructor()
+        {
+            var cyl = new Cylinder(Plane.WorldXY,1,Interval.Unit);
+            Assert.Equal(Plane.WorldXY,cyl.Plane);
+            Assert.Equal(1,cyl.Radius);
+            Assert.Equal(1,cyl.Height);
+        }
+
+        [Fact]
+        void CanCompute_PointAtCylinder()
+        {
+            var cyl = new Cylinder(Plane.WorldXY,1,Interval.Unit);
+            var actual = cyl.PointAt(0, 0);
+            var expected = new Point3d(1,0,0);
+            Assert.Equal(actual,expected);
         }
     }
 }
